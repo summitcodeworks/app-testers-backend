ad70516e3d71b690a6c779fb2a99f905ef574b4b
debarunlahiri <debarunlahiri2016@gmail.com>
2024-10-21 06:43:24 +0530

    Registration changes

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -2,25 +2,36 @@ from flask import Blueprint, request
 from app.models.user import User
 from app.extensions import db
 from app.utils.helper import create_response, user_key_required
+from datetime import datetime
 
 # Create a Blueprint instance
 bp = Blueprint('user_routes', __name__)
 
-# Use the blueprint's route method
 @bp.route('/register-user', methods=['POST'])
-@user_key_required
 def register_user():
     data = request.json
+
+    existing_user = User.query.filter(
+        (User.user_email == data['user_email']) | (User.user_key == data['user_key'])
+    ).first()
+
+    if existing_user:
+        return create_response(409, "User already exists", existing_user.to_dict())  # 409 Conflict
+
+    # If user does not exist, proceed to create a new user
     new_user = User(
         user_key=data['user_key'],
         user_name=data['user_name'],
         user_email=data['user_email'],
-        user_creation_date=data['user_creation_date'],
+        user_creation_date=datetime.utcnow(),  # Set creation date to now
         use_flag=data.get('use_flag', True)  # Default to True if not provided
     )
+
     db.session.add(new_user)
     db.session.commit()
+
     return create_response(201, "New user created successfully", new_user.to_dict())
+
 
 @bp.route('/users', methods=['GET'])
 @user_key_required
